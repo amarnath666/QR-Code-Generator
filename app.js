bea8dc4025402ea7da1b9ae69ae13baf9b17a011
Amarnath Dhumal <145348741+amarnath666@users.noreply.github.com>
2023-11-09 08:36:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let btn = document.querySelector("button");
+let qrText = document.getElementById("qrText")
+let qrImage = document.getElementById("qrImage");
+// let imgBox = document.getElementById("imgBox");
+
+btn.addEventListener ("click", async () => {
+    await getCode();
+});
+
+async function getCode () {
+    try {
+        qrImage.src ="https://api.qrserver.com/v1/create-qr-code/?size=100x100&data="+qrText.value;
+    } catch (err) {
+        console.log("Error -", err);
+    }  
+}
+
